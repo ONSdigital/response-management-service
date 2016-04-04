3e43391f0bc0b9319b84e5d869d0b94ad6709510
Chris Parker <chris.j.parker@ons.gsi.gov.uk>
2016-04-04 13:29:32 +0100

    CTPA-336 reinstate FeedbackServiceTest

--- actionsvc/src/test/java/uk/gov/ons/ctp/response/action/message/FeedbackServiceTest.java
+++ actionsvc/src/test/java/uk/gov/ons/ctp/response/action/message/FeedbackServiceTest.java
@@ -17,69 +17,70 @@ import org.springframework.integration.channel.QueueChannel;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.PollableChannel;
-import org.springframework.messaging.SubscribableChannel;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.integration.samples.testing.gateway.VoidGateway;
 
-//@ContextConfiguration (locations = { "/integration-context.xml" })  // commented out as using this configuration generates an error on startup
-//@ContextConfiguration (locations = { "FeedbackServiceTest-context.xml" })
-//@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration (locations = { "FeedbackServiceTest-context.xml" })
+//@ContextConfiguration (locations = { "/integration-context.xml" })  
+@RunWith(SpringJUnit4ClassRunner.class)
 public class FeedbackServiceTest {
-
+  
+  public FeedbackService feedbackService = new FeedbackService();
+  
   @Before
   public void setUp() throws Exception {
-    // TODO mock ActiveMQ or embedded ActiveMQ required
+    System.out.println("Test");
   }
 
   @After
   public void tearDown() throws Exception {
   }
 
-//  @Autowired
-//  MessageChannel feedbackXml;
-//
-//  @Autowired
-//  MessageChannel feedbackTransformed;
-//  
-//  @Autowired
-//  QueueChannel feedbackXmlValid;
-////  QueueChannel feedbackTransformed;
-////  SubscribableChannel feedbackTransformed;
-////  MessageChannel feedbackTransformed; 
-////  PollableChannel feedbackTransformed;
-////  VoidGateway feedbackTransformed;
-//  
+  @Autowired
+  MessageChannel feedbackXml;
+
+  @Autowired
+  QueueChannel feedbackTransformed; 
+  
+  @Autowired
+  @Qualifier("feedbackUnmarshaller")
+  Jaxb2Marshaller feedbackMarshaller;
+  
   @Test
-  public void testOne() {
+  public void testSendctiveMQMessage() {
     try {
+            
+      String testMessage = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+        "<p:actionFeedback xmlns:p=\"http://ons.gov.uk/ctp/response/action/message/feedback\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://ons.gov.uk/ctp/response/action/message/feedback inboundMessage.xsd \">" +
+        "<actionId>1</actionId>" +
+        "<situation>situation</situation>" + 
+        "<isComplete>true</isComplete>" +
+        "<isFailed>true</isFailed>" + 
+        "<notes>notes</notes>" + 
+        "</p:actionFeedback>";            
+
+      feedbackXml.send(MessageBuilder.withPayload(testMessage).build());  
       
-      System.out.println("testOne does nothing");
+      Message<?> outMessage = feedbackTransformed.receive(0);
+      assertNotNull("outMessage should not be null", outMessage);
+      boolean payLoadContainsAdaptor = outMessage.getPayload().toString().contains("uk.gov.ons.ctp.response.action.message.feedback.ActionFeedback");
+      assertTrue("Payload does not contain reference to ActionFeedback adaptor", payLoadContainsAdaptor);
+      assertThat(outMessage, hasHeaderKey("timestamp"));
+      assertThat(outMessage, hasHeaderKey("id"));
+      outMessage = feedbackTransformed.receive(0);
+      assertNull("Only one message expected from feedbackTransformed", outMessage); 
+     
       
-//      String testMessage = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-//        "<p:actionFeedback xmlns:p=\"http://ons.gov.uk/ctp/response/action/message/feedback\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://ons.gov.uk/ctp/response/action/message/feedback inboundMessage.xsd \">" +
-//        "<actionId>1</actionId>" +
-//        "<situation>situation</situation>" + 
-//        "<isComplete>true</isComplete>" +
-//        "<isFailed>true</isFailed>" + 
-//        "<notes>notes</notes>" + 
-//        "</p:actionFeedback>";
-//      
-//      feedbackXml.send(MessageBuilder.withPayload(testMessage).build());
-//      Message<?> outMessage = feedbackXmlValid.receive(0);
-//      assertNotNull("outMessage should not be null", outMessage);
-//      boolean payLoadContainsAdaptor = outMessage.getPayload().toString().contains("uk.gov.ons.ctp.response.action.message.feedback.ActionFeedback");
-//      assertTrue("Payload does not contain reference to ActionFeedback adaptor", payLoadContainsAdaptor);
-//      assertThat(outMessage, hasHeaderKey("timestamp"));
-//      assertThat(outMessage, hasHeaderKey("id"));
-//      outMessage = feedbackXmlValid.receive(0);
-//      assertNull("Only one message expected", outMessage);  
+      String contextPath = feedbackMarshaller.getContextPath();
+      String jaxbContext = feedbackMarshaller.getJaxbContext().toString();
       
+      System.out.println("Marshaller context path: " + contextPath);
+      System.out.println("Marshaller jaxbContext: " + jaxbContext);
 
     }
     catch (Exception ex) {
-      fail("testOne has failed " + ex.getMessage());
+      fail("testSendctiveMQMessage has failed " + ex.getMessage());
     }
     
   }
