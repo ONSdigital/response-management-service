374f3056e60dcf44f84318f53f26bfaaced8a2fa
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-04-19 14:39:08 +0100

    CTPA-239 review comments/changes

--- actionsvc/src/main/java/uk/gov/ons/ctp/response/action/scheduled/ActionDistributor.java
+++ actionsvc/src/main/java/uk/gov/ons/ctp/response/action/scheduled/ActionDistributor.java
@@ -1,9 +1,6 @@
 package uk.gov.ons.ctp.response.action.scheduled;
 
-import org.springframework.scheduling.annotation.Scheduled;
-
 public interface ActionDistributor {
-
   /**
    * The scheduled wake up method - this is where it all kicks off.
    */
