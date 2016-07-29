dd4a263e160a9cb1c75ea3c57fd750c633cf4b6a
Phil Whiles <philip.whiles@ons.gov.uk>
2016-07-29 12:56:56 +0100

    CTPA-526 tidy

--- actionsvc/src/main/java/uk/gov/ons/ctp/response/action/scheduled/distribution/ActionDistributor.java
+++ actionsvc/src/main/java/uk/gov/ons/ctp/response/action/scheduled/distribution/ActionDistributor.java
@@ -1,11 +1,8 @@
 package uk.gov.ons.ctp.response.action.scheduled.distribution;
 
 import java.math.BigInteger;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
 import java.util.stream.Collectors;
 
