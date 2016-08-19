90c1babc964cbbe18aa54c933a34bc07e209886d
Martin Humphrey <martin.humphrey@ons.gov.uk>
2016-08-19 16:23:51 +0100

    CTPA-619 - Modify Action service to consume Case queue
    Tidy up with Switch statement

--- actionsvc/src/main/java/uk/gov/ons/ctp/response/action/service/impl/CaseNotificationServiceImpl.java
+++ actionsvc/src/main/java/uk/gov/ons/ctp/response/action/service/impl/CaseNotificationServiceImpl.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import uk.gov.ons.ctp.response.action.domain.model.ActionCase;
 import uk.gov.ons.ctp.response.action.domain.repository.ActionCaseRepository;
 import uk.gov.ons.ctp.response.action.service.CaseNotificationService;
-import uk.gov.ons.ctp.response.casesvc.message.notification.NotificationType;
 
 /**
  * Save to Action.Case table for case creation life cycle events, delete for
@@ -26,12 +25,16 @@ public class CaseNotificationServiceImpl implements CaseNotificationService {
   @Override
   public void acceptNotification(List<ActionCase> cases) {
     cases.forEach((caseMessage) -> {
-      if (caseMessage.getNotificationType() == NotificationType.CREATED) {
-        actionCaseRepo.save(caseMessage);
-      } else if (caseMessage.getNotificationType() == NotificationType.CLOSED) {
-        actionCaseRepo.delete(caseMessage);
-      } else {
-        log.warn("Unknown Case lifecycle event {}", caseMessage.getNotificationType());
+      switch (caseMessage.getNotificationType()) {
+        case CREATED:
+          actionCaseRepo.save(caseMessage);
+          break;
+        case CLOSED:
+          actionCaseRepo.delete(caseMessage);
+          break;
+        default:
+          log.warn("Unknown Case lifecycle event {}", caseMessage.getNotificationType());
+          break;
       }
     });
     actionCaseRepo.flush();
