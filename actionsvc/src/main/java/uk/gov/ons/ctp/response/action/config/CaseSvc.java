bd2a5f0e2c2b0cf9f3fe257c1f7bfe0dcc3c7bb3
Phil Whiles <philip.whiles@ons.gov.uk>
2016-12-15 10:52:46 +0000

    CTAP-980 Added basic auth to iac connection config

--- actionsvc/src/main/java/uk/gov/ons/ctp/response/action/config/CaseSvc.java
+++ actionsvc/src/main/java/uk/gov/ons/ctp/response/action/config/CaseSvc.java
@@ -4,7 +4,8 @@ import lombok.Data;
 import uk.gov.ons.ctp.common.rest.RestClientConfig;
 
 /**
- * App config POJO for Case service access - host/location and endpoint locations
+ * App config POJO for Case service access - host/location and endpoint
+ * locations
  *
  */
 @Data
