94e157253676081d86bf57850af12c13a44da0ae
Martin Humphrey <martin.humphrey@ons.gov.uk>
2017-01-30 14:36:00 +0000

    CTPA-580 Add contact details to initialPrint tempate

--- actionexportersvc/src/main/resources/database/changes/release-9.33.3/actionexport_2017_update_initialprint.sql
+++ actionexportersvc/src/main/resources/database/changes/release-9.33.3/actionexport_2017_update_initialprint.sql
@@ -1,3 +1,9 @@
-<#list actionRequests as actionRequest>
+-- Update InitialPrint template
+
+SET SCHEMA 'actionexporter';
+
+UPDATE template SET content = '<#list actionRequests as actionRequest>
 ${actionRequest.iac?trim}|${(actionRequest.caseRef)!}|${(actionRequest.address.organisationName)!}|${(actionRequest.contact.title)!}|${(actionRequest.contact.forename)!}|${(actionRequest.contact.surname)!}|${(actionRequest.address.line1)!}|${(actionRequest.address.line2)!}|${(actionRequest.address.locality)!}|${(actionRequest.address.townName)!}|${(actionRequest.address.postcode)!}
 </#list>
+'
+WHERE name = 'initialPrint';
