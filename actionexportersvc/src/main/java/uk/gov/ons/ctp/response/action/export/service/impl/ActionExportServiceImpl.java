28aec0b225732a0a0b938376e98e4cd600d739e3
Philippe Brossier <brossierp@gmail.com>
2017-05-03 15:44:32 +0100

    ctpa-1252 correcting the endpoints which accept a file

--- actionexportersvc/src/main/java/uk/gov/ons/ctp/response/action/export/service/impl/ActionExportServiceImpl.java
+++ actionexportersvc/src/main/java/uk/gov/ons/ctp/response/action/export/service/impl/ActionExportServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,6 +39,7 @@ public class ActionExportServiceImpl implements ActionExportService {
   @Autowired
   private ActionFeedbackPublisher actionFeedbackPubl;
 
+  @Qualifier("actionExporterBeanMapper")
   @Autowired
   private MapperFacade mapperFacade;
 
